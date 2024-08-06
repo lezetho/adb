fc44723ed43f84d408d138712fd389564400fda9
SuperEvilLuke <97245327+SuperEvilLuke@users.noreply.github.com>
2024-08-06 23:27:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function backupDatabases() {
             return;
         }
         console.log(`Panel backup completed: ${stdout}`);
-        sendDiscordWebhook(`Panel backup completed: /database/panel/panel-${date}-${time}.sql`);  // edit acordingly for your webhook message
+        sendDiscordWebhook(`Panel backup completed: /database/panel/panel-${date}-${time}.sql`);  // Edit acordingly for your webhook message
     });
 
     exec(controlPanelBackup, (error, stdout, stderr) => {
@@ -29,7 +29,7 @@ function backupDatabases() {
             return;
         }
         console.log(`Controlpanel backup completed: ${stdout}`);
-        sendDiscordWebhook(`Controlpanel backup completed: /database/controlpanel/controlpanel-${date}-${time}.sql`); // edit acordingly for your webhook message
+        sendDiscordWebhook(`Controlpanel backup completed: /database/controlpanel/controlpanel-${date}-${time}.sql`); // Edit acordingly for your webhook message
     });
 }
 
