c7a2dc34362573f617913692d1c9191a7fb5f3e6
SuperEvilLuke <97245327+SuperEvilLuke@users.noreply.github.com>
2024-08-11 00:30:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,24 +13,27 @@ function backupDatabases() {
     const { date, time } = getCurrentDateTime();
     const panelBackup = `mysqldump -u root --opt panel > /database/panel/panel-${date}-${time}.sql`; // Adjust this to your database and where you would like to to dump
     const controlPanelBackup = `mysqldump -u root --opt controlpanel > /database/controlpanel/controlpanel-${date}-${time}.sql`; // You may add more or remove this if you wish
+
     // Make sure you edit panelBackup to whatever you set above (line 14)
     exec(panelBackup, (error, stdout, stderr) => {
         if (error) {
-            console.error(`Error executing panel backup: ${error}`);
+            console.error(`Error executing panel backup: ${error.message}\nCode: ${error.code}\nSignal: ${error.signal}`);
+            sendDiscordWebhook(`Error executing panel backup: ${stderr}`);
             return;
         }
         console.log(`Panel backup completed: ${stdout}`);
-        sendDiscordWebhook(`Panel backup completed: /database/panel/panel-${date}-${time}.sql`);  // Edit acordingly for your webhook message
+        sendDiscordWebhook(`Panel backup completed: /database/panel/panel-${date}-${time}.sql`);  // Edit accordingly for your webhook message
     });
-    
-     // Make sure you edit panelBackup to whatever you set above (line 15)
+
+    // Make sure you edit controlPanelBackup to whatever you set above (line 15)
     exec(controlPanelBackup, (error, stdout, stderr) => {
         if (error) {
-            console.error(`Error executing controlpanel backup: ${error}`);
+            console.error(`Error executing controlpanel backup: ${error.message}\nCode: ${error.code}\nSignal: ${error.signal}`);
+            sendDiscordWebhook(`Error executing controlpanel backup: ${stderr}`);
             return;
         }
         console.log(`Controlpanel backup completed: ${stdout}`);
-        sendDiscordWebhook(`Controlpanel backup completed: /database/controlpanel/controlpanel-${date}-${time}.sql`); // Edit acordingly for your webhook message
+        sendDiscordWebhook(`Controlpanel backup completed: /database/controlpanel/controlpanel-${date}-${time}.sql`); // Edit accordingly for your webhook message
     });
 }
 
@@ -51,7 +54,6 @@ function sendDiscordWebhook(message) {
             console.error('Error sending Discord webhook:', error);
         });
 }
-
 
 schedule.scheduleJob('0 */3 * * *', backupDatabases);
 
